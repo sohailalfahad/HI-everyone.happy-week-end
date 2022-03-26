2e8e2a3aa9567a6f6db25ce49c91eaa587d6b9af
sohailalfahad <102410147+sohailalfahad@users.noreply.github.com>
2022-03-26 14:47:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+#Defining a function called main()
+def main():
+
+    i = 1
+    max = 10
+    while (i<max):# when program reachesthis condition it will stop execution 10<10
+        print(i)
+        i=i+1
+
+#calling the function
+main()
